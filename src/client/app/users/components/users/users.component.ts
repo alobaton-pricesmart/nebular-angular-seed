b30722e5ff984604c810c83145266de0db5850e8
alobaton <alobaton@innova4j.com>
2019-02-04 09:59:03 -0500

    Delete unnecessary code.

--- src/client/app/users/components/users/users.component.ts
+++ src/client/app/users/components/users/users.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     moduleId: module.id,
@@ -55,7 +54,7 @@ export class UsersComponent implements OnInit {
 
     source: any;
 
-    constructor(private modalService: NgbModal) {
+    constructor() {
         this.source = new LocalDataSource([]);
     }
 
